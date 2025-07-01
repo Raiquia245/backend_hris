b3ad899cf57ba0d5da5b2b184d18b43dacb7232b
Valdez <bentirazv@gmail.com>
2025-07-01 09:58:27 +0700

    Initial commit

---
+++ src/main/java/valdez/backend_hris/BackendHrisApplication.java
@@ -0,0 +1,13 @@
+package valdez.backend_hris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/valdez/backend_hris/BackendHrisApplicationTests.java
@@ -0,0 +1,13 @@
+package valdez.backend_hris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
